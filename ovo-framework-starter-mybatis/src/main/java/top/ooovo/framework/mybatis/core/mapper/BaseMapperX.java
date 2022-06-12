889551fff6d7334ef90694adabfeaa46ea9e0aa4
QAQ <2436781725@qq.com>
2022-06-12 19:18:11 +0800

    fix mybatis-plus version

--- ovo-framework-starter-mybatis/src/main/java/top/ooovo/framework/mybatis/core/mapper/BaseMapperX.java
+++ ovo-framework-starter-mybatis/src/main/java/top/ooovo/framework/mybatis/core/mapper/BaseMapperX.java
@@ -1,14 +1,17 @@
 package top.ooovo.framework.mybatis.core.mapper;
 
 import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
 import org.apache.ibatis.annotations.Param;
 import top.ooovo.framework.common.pojo.PageParam;
 import top.ooovo.framework.common.pojo.PageResult;
 import top.ooovo.framework.mybatis.util.MybatisUtils;
 
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -24,22 +27,41 @@ public interface BaseMapperX<T> extends BaseMapper<T> {
      * @param queryWrapper 查询条件
      * @return 分页结果
      */
+
     default PageResult<T> selectPage(PageParam pageParam, @Param("ew") Wrapper<T> queryWrapper) {
+        // MyBatis Plus 查询
         IPage<T> mpPage = MybatisUtils.buildPage(pageParam);
         selectPage(mpPage, queryWrapper);
-        return  new PageResult<>(mpPage.getRecords(), mpPage.getTotal());
+        // 转换返回
+        return new PageResult<>(mpPage.getRecords(), mpPage.getTotal());
     }
 
     default T selectOne(String field, Object value) {
         return selectOne(new QueryWrapper<T>().eq(field, value));
     }
 
+    default T selectOne(SFunction<T, ?> field, Object value) {
+        return selectOne(new LambdaQueryWrapper<T>().eq(field, value));
+    }
+
     default T selectOne(String field1, Object value1, String field2, Object value2) {
         return selectOne(new QueryWrapper<T>().eq(field1, value1).eq(field2, value2));
     }
 
-    default Integer selectCount(String field, Object value) {
+    default T selectOne(SFunction<T, ?> field1, Object value1, SFunction<T, ?> field2, Object value2) {
+        return selectOne(new LambdaQueryWrapper<T>().eq(field1, value1).eq(field2, value2));
+    }
+
+    default Long selectCount() {
+        return selectCount(new QueryWrapper<T>());
+    }
+
+    default Long selectCount(String field, Object value) {
         return selectCount(new QueryWrapper<T>().eq(field, value));
+    }
+
+    default Long selectCount(SFunction<T, ?> field, Object value) {
+        return selectCount(new LambdaQueryWrapper<T>().eq(field, value));
     }
 
     default List<T> selectList() {
@@ -49,4 +71,33 @@ public interface BaseMapperX<T> extends BaseMapper<T> {
     default List<T> selectList(String field, Object value) {
         return selectList(new QueryWrapper<T>().eq(field, value));
     }
+
+    default List<T> selectList(SFunction<T, ?> field, Object value) {
+        return selectList(new LambdaQueryWrapper<T>().eq(field, value));
+    }
+
+    default List<T> selectList(String field, Collection<?> values) {
+        return selectList(new QueryWrapper<T>().in(field, values));
+    }
+
+    default List<T> selectList(SFunction<T, ?> field, Collection<?> values) {
+        return selectList(new LambdaQueryWrapper<T>().in(field, values));
+    }
+
+    /**
+     * 逐条插入，适合少量数据插入，或者对性能要求不高的场景
+     *
+     * 如果大量，请使用 {@link com.baomidou.mybatisplus.extension.service.impl.ServiceImpl#saveBatch(Collection)} 方法
+     * 使用示例，可见 RoleMenuBatchInsertMapper、UserRoleBatchInsertMapper 类
+     *
+     * @param entities 实体们
+     */
+    default void insertBatch(Collection<T> entities) {
+        entities.forEach(this::insert);
+    }
+
+    default void updateBatch(T update) {
+        update(update, new QueryWrapper<>());
+    }
+
 }
